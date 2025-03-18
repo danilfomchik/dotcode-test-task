033ec458a86c03263f0d3dcdc392b997535b2560
Daniil Fomenko <danil.fomchik@gmail.com>
2025-03-18 09:56:12 +0200

    common changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {createRoot} from 'react-dom/client';
 import {RouterProvider} from 'react-router';
 
 import './index.css';
-import ReduxProvider from './redux/redux-provider';
+import ReduxProvider from './providers/redux-provider';
 import {router} from './routes/router';
 
 createRoot(document.getElementById('root')!).render(
